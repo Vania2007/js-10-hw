c4dcb3b15456f243490dd83efcc2810ea7cde1af
Vania2007 <momotis2007@gmail.com>
2024-04-03 17:42:49 +0300

    spam-update

--- spam.js
+++ spam.js
@@ -3,6 +3,9 @@ function isStringSpam(str) {
   str = str.toLowerCase();
   if (str.includes("spam")) {
     alert(str.replaceAll("spam", "***"));
+    return true;
+  } else {
+    return false;
   }
 }
 isStringSpam(str);
